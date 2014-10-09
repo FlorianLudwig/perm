440c00504552980a0351dec198d3fe0a5f929333
Florian Ludwig <f.ludwig@greyrook.com>
2014-10-09 15:05:53 +0200

    add meta infos

--- setup.py
+++ setup.py
@@ -9,4 +9,14 @@ setup(
     packages=['perm'],
     zip_safe=False,
     include_package_data=True,
+    license="http://www.apache.org/licenses/LICENSE-2.0",
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
 )
